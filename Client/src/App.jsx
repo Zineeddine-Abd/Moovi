0c535e3f31b4852b856a40578ac41c04f491236c
Abdeladim Zine eddine <zineeddine.abdeladim@gmail.com>
2025-08-18 17:47:43 +0100

    init project repo commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return(
+    <div className='flex justify-center items-center w-full h-screen'>
+      <div className='bg-blue-300 w-[300px] h-[200px]'>
+        <p>Hello</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
