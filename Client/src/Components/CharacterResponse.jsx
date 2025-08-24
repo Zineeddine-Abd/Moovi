989a3a6742c66083691a0179d49d9ddb8170271a
Abdeladim Zine eddine <zineeddine.abdeladim@gmail.com>
2025-08-24 22:29:28 +0100

    changed the character response font to arial

--- Client/src/Components/CharacterResponse.jsx
+++ Client/src/Components/CharacterResponse.jsx
@@ -7,7 +7,7 @@ export default function CharacterResponse({ text }) {
 
   return (
     <div className="w-full max-w-2xl mb-6 p-4 bg-[#1A1A1D] border border-[#333] rounded-2xl animate-fade-in">
-      <p className="text-gray-300 font-rajdhani text-lg">
+      <p className="text-gray-300 font-arial text-lg">
         {text}
       </p>
     </div>
